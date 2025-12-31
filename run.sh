9f2006087d2e4c85b8205c3d009d40ffec327c04
ajiho <lujiahao@88.com>
2025-12-31 15:22:20 +0800

    fix: adfs
    Signed-off-by: ajiho <lujiahao@88.com>

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-GITHUB_OUTPUT_FILE="$1"
-
-
 # 提取 tag 名，去掉 refs/tags/
 REF="${GITHUB_REF#refs/tags/}"
 
@@ -12,12 +9,12 @@ if [[ "$REF" =~ - ]]; then
   ID="${REF#*-}"
   ID="${ID%%.*}"  # alpha.0 -> alpha
   echo "Detected tag: $ID"
-  echo "tag=$ID" >> "$GITHUB_OUTPUT_FILE"
+  echo "tag=$ID" >>$GITHUB_OUTPUT
   echo "prerelease: true"
-  echo "prerelease=true" >> "$GITHUB_OUTPUT_FILE"
+  echo "prerelease=true" >>$GITHUB_OUTPUT
 else
   echo "Detected tag: latest"
-  echo "tag=latest" >> "$GITHUB_OUTPUT_FILE"
+  echo "tag=latest" >>$GITHUB_OUTPUT
   echo "prerelease: false"  
-  echo "prerelease=false" >> "$GITHUB_OUTPUT_FILE"
+  echo "prerelease=false" >>$GITHUB_OUTPUT
 fi
