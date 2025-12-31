dfba901d38c735c82e0519db3412261c5b7fab2a
ajiho <lujiahao@88.com>
2025-12-31 15:05:03 +0800

    fix: aa
    Signed-off-by: ajiho <lujiahao@88.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+GITHUB_OUTPUT_FILE="$1"
+
+
 # 提取 tag 名，去掉 refs/tags/
 REF="${GITHUB_REF#refs/tags/}"
 
@@ -9,12 +12,12 @@ if [[ "$REF" =~ - ]]; then
   ID="${REF#*-}"
   ID="${ID%%.*}"  # alpha.0 -> alpha
   echo "Detected tag: $ID"
-  echo "tag=$ID" >> $GITHUB_OUTPUT
+  echo "tag=$ID" >> "$GITHUB_OUTPUT_FILE"
   echo "prerelease: true"
-  echo "prerelease=true" >> $GITHUB_OUTPUT
+  echo "prerelease=true" >> "$GITHUB_OUTPUT_FILE"
 else
   echo "Detected tag: latest"
-  echo "tag=latest" >> $GITHUB_OUTPUT
+  echo "tag=latest" >> "$GITHUB_OUTPUT_FILE"
   echo "prerelease: false"  
-  echo "prerelease=false" >> $GITHUB_OUTPUT
+  echo "prerelease=false" >> "$GITHUB_OUTPUT_FILE"
 fi
